ddbf732242a21ed8380aec90f2edbfd31edd848d
Meet Maheta <149992254+mmb0710@users.noreply.github.com>
2024-04-05 02:16:19 -0400

    Add files via upload

---
+++ app/src/main/java/algonquin/cst2335/mahe0111/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.mahe0111;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
